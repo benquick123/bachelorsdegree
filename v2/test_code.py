90043ffbed0f1a629a937b6baa2212e29d6f4a94
benquick <benjamin.fele@radiostudent.si>
2017-08-27 00:57:50 +0200

    optimal attr num (articles) - random forest

--- v2/test_code.py
+++ v2/test_code.py
@@ -51,7 +51,7 @@ def optimal_attr_number(plot=False, **kwargs):
     while i < n_iter:
         # threshold = threshold_range[0] + np.random.rand() * (threshold_range[1] - threshold_range[0])
         threshold = threshold_range[0] + i/n_iter * (threshold_range[1] - threshold_range[0])
-        feature_selector = SelectFromModel(LinearSVC(), threshold=str(threshold) + "*mean")
+        feature_selector = SelectFromModel(RandomForestClassifier(), threshold=str(threshold) + "*mean")
 
         # threshold = threshold_range[0] + (i / n_iter) * (threshold_range[1] - threshold_range[0])
         _, score, precision, recall, matrix_shape, _ = train_f(feature_selector=feature_selector, model=model, data_X=data_X, data_Y=data_Y, type=type, dates=dates, save=False, learn=True, test=False)
