8bbd376baf85cceac14b1e809c541629f202a3fc
benquick <benjamin.fele@radiostudent.si>
2017-08-27 20:41:37 +0200

    articles model hiperparams

--- v2/__init__.py
+++ v2/__init__.py
@@ -555,7 +555,7 @@ def __init__():
     # initial_load()
 
     functions = [parameter_search.randomized_data_params_search]
-    functions = [test_code.optimal_attr_number]
+    functions = [test_code.randomized_model_params_search]
 
     window = 6600
     margin = 0.00967742793041
